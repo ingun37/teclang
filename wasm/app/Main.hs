23d03ad0a616f7ae536f1e61cd108b68e17bd5e8
ingun <ingun37@gmail.com>
2025-12-11 11:22:20 +0900

    add formatHaskell

--- wasm/app/Main.hs
+++ wasm/app/Main.hs
@@ -1,19 +1,20 @@
 module Main (main) where
 
+import Data.Aeson qualified as J
 import Data.ByteString qualified as B
 import Data.ByteString.Unsafe qualified as BU
 import Data.Text qualified as T
 import Data.Text.Encoding qualified as TE
+import Data.Text.Lazy qualified as TL
 import Foreign.C (CString)
 import Foreign.C.String
 import Foreign.C.Types
 import Foreign.Ptr
-import MyLib qualified ()
 import GHC.Internal.Foreign.Marshal.Alloc qualified as Alloc
 import GHC.Wasm.Prim
-import Data.Aeson qualified as J
+import MyLib qualified ()
+import Text.Pretty.Simple qualified as Simple
 import Util qualified
-import qualified Util
 
 main :: IO ()
 main = do
@@ -30,6 +31,7 @@ fibo n = do
 
 -- FFI wrapper to accept a C-style string pointer and length
 foreign export ccall processString :: Ptr CChar -> CInt -> IO (Ptr CChar)
+
 processString :: Ptr CChar -> CInt -> IO (Ptr CChar)
 processString ptr len = do
   -- 1. Convert the C-style string (Ptr CChar + length) into a Haskell ByteString
@@ -45,10 +47,11 @@ processString ptr len = do
   let resultString = T.unpack result_text
   -- 5. Convert the ByteString to a NUL-terminated CString
   --    (You MUST ensure this memory is managed/freed by the host later)
-  (ptr,len) <- newCStringLen resultString
+  (ptr, len) <- newCStringLen resultString
   return ptr
 
 foreign export javascript "jsTest" jsTest :: JSString -> IO JSString
+
 jsTest :: JSString -> IO JSString
 jsTest jsval = do
   putStrLn "jsval called!"
@@ -58,3 +61,7 @@ jsTest jsval = do
 foreign export javascript "parseHaskell" parseHaskell :: JSString -> IO JSString
 parseHaskell :: JSString -> IO JSString
 parseHaskell code = toJSString <$> Util.parseHaskell (fromJSString code)
+
+foreign export javascript "formatHaskell" formatHaskell :: JSString -> IO JSString
+formatHaskell :: JSString -> IO JSString
+formatHaskell = return . toJSString . TL.unpack . Simple.pStringNoColor . fromJSString
