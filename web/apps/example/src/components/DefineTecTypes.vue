4ed2b930526f8a1afa591c26bc12fc6d7861f764
Ingun Jon <ingun37@gmail.com>
2025-12-26 15:41:47 +0900

    Grid system DefineTecTypes.vue

--- web/apps/example/src/components/DefineTecTypes.vue
+++ web/apps/example/src/components/DefineTecTypes.vue
@@ -10,11 +10,19 @@ function addTecType() {
 </script>
 
 <template>
-  <DefineTecType v-for="(_, i) in model" v-model="model[i]!" />
+  <v-container fluid>
+    <v-row>
+      <v-col v-for="(_, i) in model" :key="i" cols="12">
+        <DefineTecType v-model="model[i]!" />
+      </v-col>
 
-  <v-btn prepend-icon="mdi-plus" variant="tonal" @click="addTecType">
-    Add
-  </v-btn>
+      <v-col cols="12">
+        <v-btn prepend-icon="mdi-plus" variant="tonal" @click="addTecType">
+          Add
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped lang="sass"></style>
