cbabbf4a4dd54b7c3b9b92f304105f8b1963942a
Ingun Jon <ingun37@gmail.com>
2025-12-28 23:43:17 +0900

    initial code

--- web/apps/example/src/pages/index.vue
+++ web/apps/example/src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-row ga-2">
     <v-btn @click="fillSampleCode">fill sample code</v-btn>
-    <v-btn @click="updateTypeAST">update</v-btn>
+    <v-btn @click="updateUI">update ui</v-btn>
   </div>
   <v-textarea
     v-model="haskellCode"
@@ -20,20 +20,28 @@ const tecType = ref<C.TecType.UniqueTecType | null>(null);
 const haskellCode = ref("");
 
 const sampleHaskellCode = `\
-data Enum0 = A
-           | B
-           | C
-
-data TecType = AType Enum0 String`;
+data Side = Front
+          | Back
+          | Left
+          | Right
+data Article = A0 | A1
+data TecType = Render Side Article`;
 
 function fillSampleCode() {
   haskellCode.value = sampleHaskellCode;
 }
-async function updateTypeAST() {
+async function updateUI() {
   tecType.value = C.jsonToUniqueTecType(
     JSON.parse(
       await useAppStore().haskell!.exports.encodeHaskellType(haskellCode.value),
     ),
   );
 }
+
+watch(tecType, async (newTecType) => {
+  if (newTecType)
+    haskellCode.value = await useAppStore().haskell!.exports.decodeHaskellType(
+      JSON.stringify(C.uniqueTecTypeToJson(newTecType)),
+    );
+});
 </script>
