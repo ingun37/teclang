83d804bff83459b4c9ac03cc7ab5dbe88a84205c
Ingun Jon <ingun37@gmail.com>
2026-01-02 22:09:35 +0900

    rename classes -> tecValues

--- wasm/src/TecTypes.hs
+++ wasm/src/TecTypes.hs
@@ -15,7 +15,7 @@ instance ToJSON TecValue where
 
 instance FromJSON TecValue
 
-data TecSum = TecSum {tecTypeName :: String, classes :: [TecValue]}
+data TecSum = TecSum {tecTypeName :: String, tecValues :: [TecValue]}
   deriving (Show, Generic)
 
 instance ToJSON TecSum where
