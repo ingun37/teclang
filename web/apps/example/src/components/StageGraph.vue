a71d0eab3a67e72167317a8481dbe32cf8c96c58
Ingun Jon <ingun37@gmail.com>
2026-02-21 17:45:31 +0900

    svg

--- web/apps/example/src/components/StageGraph.vue
+++ web/apps/example/src/components/StageGraph.vue
@@ -2,9 +2,7 @@
 import { DirectedGraph } from "graphology";
 import * as C from "codec";
 import * as E from "effect";
-import Sigma from "sigma";
-import type { NodeDisplayData } from "sigma/types";
-import { random } from "graphology-layout";
+import { SVG } from "@svgdotjs/svg.js";
 
 type MyEnumA = {
   tag: "enum";
@@ -43,6 +41,9 @@ function makeEnumNodeAttr(tecEnum: C.TecEnum.TecEnum) {
   };
 }
 const sigmaContainer = useTemplateRef("sigma-container");
+const sheetWidth = ref<number>(1000);
+const sheetHeight = ref<number>(2000);
+
 function createMyGraph(): MyGraph {
   const G: MyGraph = new DirectedGraph();
   const findClass = C.help.makeFindClass(props.tecClassAst);
@@ -90,27 +91,65 @@ function createMyGraph(): MyGraph {
     );
   });
 
-  random.assign(G);
   return G;
 }
 const graph = shallowRef<MyGraph>(createMyGraph());
-
-const renderer = shallowRef<Sigma<MyNA> | null>(null);
-
+function pair<A, B>(a: A, b: B): [A, B] {
+  return [a, b];
+}
 function visualize() {
   if (sigmaContainer.value === null) throw new Error("sigmaContainer is null");
-  if (renderer.value === null) {
-    renderer.value = new Sigma(graph.value, sigmaContainer.value, {
-      nodeReducer() {
-        const res: Partial<NodeDisplayData> = {};
-        res.x = Math.random();
-        res.y = Math.random();
-        return res;
-      },
-    });
-  } else {
-    renderer.value.refresh();
-    renderer.value.setGraph(graph.value);
+
+  var draw = SVG()
+    .addTo(sigmaContainer.value)
+    .size(sheetWidth.value, sheetHeight.value);
+  let offx = 0;
+
+  const [enumNodes, pnumNodes] = E.Array.partitionMap(
+    graph.value.nodeEntries(),
+    (entry) => {
+      const a = entry.attributes;
+      if (a.tag === "enum") return E.Either.left(pair(entry.node, a));
+      else return E.Either.right(pair(entry.node, a));
+    },
+  );
+  if (E.Array.isNonEmptyArray(enumNodes)) {
+    let offy = 50;
+    E.pipe(
+      enumNodes,
+      E.Array.sortWith((x) => x[1].enum.tecEnumName, E.Order.string),
+      E.Array.groupWith(
+        (x, y) => x[1].enum.tecEnumName === y[1].enum.tecEnumName,
+      ),
+      E.Array.forEach((entries) => {
+        for (const entry of entries) {
+          draw.text(entry[1].value).attr({ x: offx, y: offy });
+          offy += 30;
+        }
+      }),
+    );
+    offx += 300;
+  }
+
+  if (E.Array.isNonEmptyArray(pnumNodes)) {
+    let offy = 50;
+
+    E.pipe(
+      pnumNodes,
+      E.Array.sortWith((x) => x[1].tecNodeSet.tecNodeClass, E.Order.string),
+      E.Array.groupWith(
+        (x, y) => x[1].tecNodeSet.tecNodeClass === y[1].tecNodeSet.tecNodeClass,
+      ),
+      E.Array.forEach((entries) => {
+        for (let i = 0; i < entries.length; i++) {
+          const eA = entries[i]![1];
+          draw
+            .text(`${eA.tecNodeSet.tecNodeClass}_${i}`)
+            .attr({ x: offx, y: offy });
+          offy += 30;
+        }
+      }),
+    );
   }
 }
 </script>
@@ -124,7 +163,7 @@ function visualize() {
         </div>
       </v-col>
       <v-col cols="12">
-        <v-sheet height="1000px" width="500px">
+        <v-sheet :height="sheetHeight" :width="sheetWidth">
           <div
             ref="sigma-container"
             class="sigma-container"
